750f412c73eacd34deb0c1db30990995a3701ec1
TH <hiennguyen.31251023496@st.ueh.edu.vn>
2026-09-21 14:40:31 +0700

    changed

---
+++ CSLT_26C1INF5090051_C2/SESSION08/CLASS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSLT_26C1INF5090051_C2.SESSION08
+{
+    internal class CLASS
+    {
+        static void nhap_mang_ngau_nhien(int[] a)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rnd.Next(10, 100);
+            }
+        }
+
+    }
+}
---
+++ CSLT_26C1INF5090051_C2/SESSION08/btslide.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Nodes;
+
+namespace CSLT_26C1INF5090051_C2.SESSION08
+{
+    internal class btslide
+    {
+        static int calAvg(int[] a)
+        {
+            int sum = 0;
+            foreach (int v in a)
+            {
+                sum += v;
+            }
+            return sum / a.Length; ;
+        }
+        public static void Main(string[] args)
+        {
+           
+        }
+        static bool searchByValue(int[] a, int x)
+        {
+            foreach (int v in a)
+                if (v == x)
+                    return true;
+            return false;
+        }
+        //find the index of an array element
+        static int searchIndex (int[]a,int x)
+        {
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == x)
+                    return i;
+            return -1;
+        }
+        //remive a specific element from an array
+    }
+}
